30896c16af357f41570cec06e146a9fb76432f8f
yeaseuleee <yeaseuleee@gmail.com>
2025-09-11 01:57:18 +0900

    추가

--- script.js
+++ script.js
@@ -5,27 +5,69 @@ let taskList = [];
 addBtn.addEventListener("click", addTask);
 
 function addTask() {
-    let taskCon = taskInput.value;
+    let task = {
+        id : randomId(),
+        taskCon : taskInput.value,
+        isComplete : false
+    }
 
-    taskList.push(taskCon)
-    
+    taskList.push(task)
     render();
 }
 
 function render() {
     let resultHtml = ``;
 
-    for(let i=0; i<taskList.length; i++){
-        resultHtml += `
+    for (let i = 0; i < taskList.length; i++) {
+        let toggleIcon = taskList[i].isComplete ? "🔙" : "✅";
+
+        if (taskList[i].isComplete == true) {
+            resultHtml += `
                 <div class="task">
-                    <div>${taskList[i]}</div>
+                    <div class="task-done">${taskList[i].taskCon}</div>
                     <div>
-                        <button type="button">✅</button>
-                        <button type="button">🗑 </button>
+                        <button type="button" onclick="toggleComp('${taskList[i].id}')">${toggleIcon}</button>
+                        <button type="button" onclick="deleteTask('${taskList[i].id}')">🗑</button>
                     </div>
                 </div>
-        `
+            `
+        } else {
+            resultHtml += `
+                <div class="task">
+                    <div>${taskList[i].taskCon}</div>
+                    <div>
+                        <button class="btn" type="button" onclick="toggleComp('${taskList[i].id}')">${toggleIcon}</button>
+                        <button class="btn" type="button" onclick="deleteTask('${taskList[i].id}')">🗑</button>
+                    </div>
+                </div>
+            `
+        }
     }
 
     document.getElementById('task-board').innerHTML = resultHtml;
+}
+
+
+function toggleComp(id) {
+    for(i=0;i<taskList.length;i++) {
+         if (taskList[i].id== id) {
+            taskList[i].isComplete = !taskList[i].isComplete;
+            break;
+         }
+    }
+    render();
+}
+
+function deleteTask(id) {
+    for(i=0;i<taskList.length;i++) {
+        if(taskList[i].id == id ) {
+            taskList.splice(i,1)
+            break
+        }
+   }
+   render();
+}
+
+function randomId() {
+  return '_' + Math.random().toString(36).substr(2, 9);
 }
